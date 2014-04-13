61e146398960b51210bdd17e5cc74059506d0049
Daniel Pozzi <bonndan76@googlemail.com>
2014-04-13 11:59:56 +0200

    fixed wrong path

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Release flow index file
  */
-require_once dirname(__DIR__) .'/vendor/autoload.php';
+require_once __DIR__ .'/vendor/autoload.php';
 
 $application = new bonndan\ReleaseFlow\Application('release-flow');
 $application->run();
